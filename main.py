dc799442457bc045a75d599b558390ee8e776356
cuehling <cuehling2@gmail.com>
2026-03-30 23:12:29 -0400

    added device dropdown menu when microphone inout needs to be chosen

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import sys, os, re
 from PyQt6 import QtWidgets, uic
 from audio_stream import AudioStream
 
+# Creates the Graphic User Interface (GUI) of the Grasshopper-Python
+# Code which simulates physical sound concepts
+    
+# Has two dropdown menus:
+# - audio source: choose between microphone input or .wav files
+# - Processor: choose what is simulated in Grasshopper
+#
+#
+# Has two buttons:
+# - Run: starts the program. Sends user input to the AudioStream Class.
+# - stop: ends the program
 
-class MyApp(QtWidgets.QMainWindow):
 
+class AudioApp(QtWidgets.QMainWindow):
 
     def __init__(self):
+     
         super().__init__()
         
         # Load the UI file
-        # Ensure 'design.ui' is in the same folder as this script
         uic.loadUi("gui.ui", self)
 
-        # Set Up Combo Boxes
+        # Set Up Dropdown menus
         self.set_up_sound_input_choice()
         self.set_up_grasshopper_input_choice()
 
@@ -21,77 +32,71 @@ class MyApp(QtWidgets.QMainWindow):
         self.run.clicked.connect(self.run_gh)
         self.stop.clicked.connect(self.stop_gh)
 
-        self.audio_stream = AudioStream()
-
+        
     # ==================Button Functions========================
+
     def run_gh(self):
         print("Running GH")
-
+        # Collect user's choices
         audio = self.sound_input_choice.currentText()
         gh = self.gh_input_choice.currentText()
 
-        
-        if audio == 'microphone input':
-            devices = self.audio_stream.query_mic_input()
-            print(devices)
-            
-            QtWidgets.QDialog()
+        # Create AudioStream and send user's choices to it
+        self.audio_stream = AudioStream()
+        self.audio_stream.audio = audio
 
-            choice, ok = QtWidgets.QInputDialog.getItem(
-            None,             # Parent (QWidget or None)
-            "Selection Menu", # Title
-            "Choose Source:", # Label
-            devices,            # The List
-            0,                # Current Index
-            False             # Editable (False = Dropdown, True = Textbox)
-            )
-            if ok and choice:
-                print(f"User chose: {choice}")
-                id = re.match(r'([\d]+): ', choice)
-                print(id.group(1))
-                self.audio_stream.device_id = id.group(1)
-                
+        # Choose Sound Source
+        if audio == 'microphone input':
+            # Query Which input device to use
+            devices = self.audio_stream.query_mic_input()
+            
+            # Sort through viable devices (work on this some more!)
+            device_list = []
+            for i, d in enumerate(devices):
+                print(f"{i}: {d['name']}")
         
-           
+                # Format devices for PyQT Use
+                index = d["index"]
+                name = d["name"]
+                device_list.append(f"{index}: "+f"{name}")
+            
+            # Choose device via dropdown menu
+            device_chosen = self.popup_query(title='Device Selection', list=device_list)
+
+            # Find device associated
+            id = re.match(r'([\d]+): ', device_chosen)
+            for device in devices:
+                if id.group(1) == device['index']:
+                    print(id.group(1))
+                    # Set device with correct
+                    self.audio_stream.device_id = device
+
+        self.audio_stream.start()
 
     def stop_gh(self):
         print("Stopping GH")
+        self.audio_stream.run = False
+
         
-        pass
-        
-    # ============= Initialization Functions ================
+    # =============== Initialization Functions ====================
 
     def set_up_sound_input_choice(self):
-        
+
         # Add Microphone Input
         self.sound_input_choice.addItem('microphone input')
 
         # Find Audio Files Folder
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        target_folder = os.path.join(script_dir, 'audio_files')
-
-        if not os.path.isdir(target_folder):
-            print(f"Error: The folder 'audio_files' does not exist in the script's directory.")
-        else:
-            contents = os.listdir(target_folder)
+        contents = self.find_folder('audio_files')
         
-        # Write Audio Files in sound input dropdown menu
         for content in contents:
             audio_file = re.match(r'([\w-]+)\.(mp3|wav)', content) 
             if audio_file:
                 self.sound_input_choice.addItem(audio_file.group(1))
     
-
     def set_up_grasshopper_input_choice(self):
 
-        # Find Grasshopper Files Folder
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        target_folder = os.path.join(script_dir, 'grasshopper')
-
-        if not os.path.isdir(target_folder):
-            print(f"Error: The folder 'grasshopper' does not exist in the script's directory.")
-        else:
-            contents = os.listdir(target_folder)
+        # Find Audio Files Folder
+        contents = self.find_folder('grasshopper')
         
         # Write Grasshopper Files in gh input dropdown menu
         for content in contents:
@@ -99,11 +104,47 @@ class MyApp(QtWidgets.QMainWindow):
             if gh_file:
                 self.gh_input_choice.addItem(gh_file.group(1))
         
+
+    # ============= Functional Functions ==================
+    def popup_query(self, **kwargs):
+        
+        QtWidgets.QDialog()
+        title = kwargs.get('title', '')
+        label = kwargs.get('label', None)
+        list = kwargs.get('list', None)
+        editable = kwargs.get('editable', False)
+        
+        choice, ok = QtWidgets.QInputDialog.getItem(
+        None,             # Parent (QWidget or None)
+        title,            # Title
+        label,            # Label
+        list,             # List
+        0,                # Current Index
+        False             # Editable (False = Dropdown, True = Textbox)
+        )
+        if ok and choice:
+            print(f"User chose: {choice}")
+            return choice
+    
+    def find_folder(self, folder):
+        # Find original location
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        # Add path to location
+        target_folder = os.path.join(script_dir, folder)
+        
+        if not os.path.isdir(target_folder):
+            print(f"Error: The folder 'audio_files' does not exist in the script's directory.")
+        else:
+            contents = os.listdir(target_folder)
+            return contents
+
+        
         
 def main():
     print("Hello from acoustic-architecture!")
     app = QtWidgets.QApplication(sys.argv)
-    window = MyApp()
+    window = AudioApp()
     window.show()
     sys.exit(app.exec())
 
